89a4e377d0380095122d6617e98c81c15d06a498
Brandon Wallace <brandonwallace403@gmail.com>
2023-03-08 17:52:23 -0500

    Adds myfile.md

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+
+const writeCards = (arr,event) => {
+    const messages = []
+    for(let i = 0; i < arr.length;i++) {
+        
+           const greetingMessage = `Thank you, ${arr[i]}, for the wonderful ${event} gift!`
+           messages.push(greetingMessage)
+          
+    }
+    return messages
+
+}
+console.log(writeCards(["Guadelupe", "Ollie","Aki"],"surprise"))
+function countDown(countdown){
+    var countdown = 11;
+    while (countdown > 0){
+        countdown--;
+        console.log(countdown);
+}
+}
